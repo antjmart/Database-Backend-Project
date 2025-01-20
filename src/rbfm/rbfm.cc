5416074a457121fad1f46a53768e98c7844d0eac
Anthony Martinez <retriblue7@gmail.com>
2025-01-20 01:45:35 -0800

    Implemented insertRecord() and helper method calcRecordSpace(), still need to implement two other helpers for insertRecord() to work

--- src/rbfm/rbfm.cc
+++ src/rbfm/rbfm.cc
@@ -1,4 +1,7 @@
 #include "src/include/rbfm.h"
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
 namespace PeterDB {
     RecordBasedFileManager &RecordBasedFileManager::instance() {
@@ -30,9 +33,86 @@ namespace PeterDB {
         return PagedFileManager::instance().closeFile(fileHandle);
     }
 
+    unsigned short RecordBasedFileManager::calcRecordSpace(const std::vector<Attribute> &recordDescriptor, const void * data) {
+        // need 4 bytes for record directory entry, 2 for offset and 2 for length
+        // need 2 more for number of non-null fields
+        unsigned short recordSpace = 6;
+        unsigned numFields = recordDescriptor.size();
+        unsigned nullFlagBytes = numFields / 8;
+        if (nullFlagBytes % 8 != 0) ++nullFlagBytes;
+        recordSpace += nullFlagBytes;
+
+        // each non-null field needs 2 bytes for offset pointer, plus data type size
+        unsigned fieldsRemaining = numFields;
+        for (unsigned i = 0, flagByte = 0; i < nullFlagBytes; ++i, flagByte = 0, fieldsRemaining -= 8) {
+            // get set of 8 null flag bits
+            memcpy(&flagByte, (const char *)data + i, 1);
+
+            for (unsigned j = 0; j < 8 && j < fieldsRemaining; ++j)
+                if (((flagByte >> j) & 1) == 0)  // null bit flag of zero means space is needed
+                    recordSpace += 2 + recordDescriptor[i * 8 + j].length;
+        }
+
+        return recordSpace;
+    }
+
+    unsigned short RecordBasedFileManager::putRecordInEmptyPage(const std::vector<Attribute> &recordDescriptor, const void * data, void * pageData, unsigned short recordSpace) {
+        return 0;
+    }
+
+    unsigned short RecordBasedFileManager::putRecordInNonEmptyPage(const std::vector<Attribute> &recordDescriptor, const void * data, void * pageData, unsigned short recordSpace) {
+        return 0;
+    }
+
     RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
                                             const void *data, RID &rid) {
-        return -1;
+        void * pageData = malloc(PAGE_SIZE);
+        unsigned pageNum;  // page which record will be inserted to, starts with last page
+        unsigned freeIndex = PAGE_SIZE / sizeof(unsigned short) - 1;
+        unsigned short recordSpace = calcRecordSpace(recordDescriptor, data);
+        unsigned short slotNum;
+
+        if (fileHandle.pageCount == 0) {
+            pageNum = 0;  // no need to check pages
+            slotNum = putRecordInEmptyPage(recordDescriptor, data, pageData, recordSpace);
+        } else {
+            unsigned short freeSpace;
+            pageNum = fileHandle.pageCount - 1;
+            if (fileHandle.readPage(pageNum, pageData) == -1) return -1;
+            freeSpace = ((unsigned short *)pageData)[freeIndex];
+
+            // if not enough space, need to start iterating through other pages
+            if (recordSpace > freeSpace) {
+                for (pageNum = 0; pageNum < fileHandle.pageCount - 1; ++pageNum) {
+                    if (fileHandle.readPage(pageNum, pageData) == -1) return -1;
+                    freeSpace = ((unsigned short *)pageData)[freeIndex];
+                    if (recordSpace <= freeSpace) break;  // stop searching if enough space
+                }
+
+                if (pageNum == fileHandle.pageCount - 1) {
+                    free(pageData);
+                    pageData = malloc(PAGE_SIZE);
+                    ++pageNum;
+                }
+            }
+
+            // now that pageNum is determined, must call function to construct new page data
+            if (pageNum >= fileHandle.pageCount)
+                slotNum = putRecordInEmptyPage(recordDescriptor, data, pageData, recordSpace);
+            else
+                slotNum = putRecordInNonEmptyPage(recordDescriptor, data, pageData, recordSpace);
+        }
+
+        // set the record id, then write back updated page with inserted record
+        rid.pageNum = pageNum;
+        rid.slotNum = slotNum;
+        RC writeStatus;
+        if (pageNum >= fileHandle.pageCount)
+            writeStatus = fileHandle.appendPage(pageData);
+        else
+            writeStatus = fileHandle.writePage(pageNum, pageData);
+        free(pageData);
+        return writeStatus;
     }
 
     RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
